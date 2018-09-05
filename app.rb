fb17817bb4c607c71390f0aeffd0297c7bf31528
Jordan Endicott <jordan.endicott823@gmail.com>
2018-09-05 14:42:46 -0400

    connected to database

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require 'sinatra'
+require 'mysql2'
+require 'aws-sdk'
+require_relative 'local_ENV.rb'
+
+
+client = Mysql2::Client.new(:host => ENV['endpoint'], :username => ENV['username'], :password => ENV['password'], :port => ENV['port'], :database => ENV['database'], :socket =>'/tmp/mysql.sock')
 
 get '/' do
   erb :login
