8d24cdf27219a8443d0405db9c6bf0e736b3dfbb
Jordan Endicott <jordan.endicott823@gmail.com>
2018-09-05 16:45:02 -0400

    did practically nothing

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require 'sinatra'
 require 'mysql2'
-require 'aws-sdk'
+
 require_relative 'local_ENV.rb'
 
-
+load 'local_ENV.rb' if File.exist?('local_ENV.rb')
 client = Mysql2::Client.new(:host => ENV['endpoint'], :username => ENV['username'], :password => ENV['password'], :port => ENV['port'], :database => ENV['database'], :socket =>'/tmp/mysql.sock')
 
 get '/' do
+  p client
   erb :login
 end
 
@@ -19,3 +20,10 @@ get '/signup' do
   erb :signup
 end
 
+post '/signup' do
+username = params[:username]
+password = params[:password]
+
+client.query("INSERT INTO `users_table`(id, username, password) VALUES(UUID(), '#{username}' '#{password}')")
+
+end
