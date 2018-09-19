f3549ccee081336fa83dbd668ca69c5f27b8eb06
Jordan Endicott <jordan.endicott823@gmail.com>
2018-09-19 13:51:59 -0400

    failing to add delete function

--- public/js/fone_book.js
+++ public/js/fone_book.js
@@ -1,17 +1,25 @@
 function searchfunk() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("searchinput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("table");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchinput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
   }
+}
+
+function deleteRows(){
+
+	isTable = document.getElementById('users_table');
+	nBoxes = document.getElementsByName('delbox');
+	for (i=nBoxes.length-1; i>=0; i--)
+		{if (nBoxes[i].checked == true){isTable.deleteRow(i+1)}}
+}	
