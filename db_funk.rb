ef79fb121b4b09ec599d4b52060571a9596a7d75
Jordan Endicott <jordan.endicott823@gmail.com>
2018-10-02 14:00:45 -0400

    .

--- db_funk.rb
+++ db_funk.rb
@@ -1,5 +1,5 @@
 require 'mysql2'
-require_relative 'local_ENV.rb'
+
 
 load 'local_ENV.rb' if File.exist?('local_ENV.rb')
 client = Mysql2::Client.new(:host => ENV['endpoint'], :username => ENV['username'], :password => ENV['password'], :port => ENV['port'], :database => ENV['database'], :socket =>'/tmp/mysql.sock')
